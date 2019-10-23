3c45388c2390de62378ff95e8892d458af9e87ce
asrafulashiq <asrafulashiq@gmail.com>
2019-10-22 20:33:12 -0400

    /

--- tmp_run.sh
+++ tmp_run.sh
@@ -1,4 +1,6 @@
-conda activate torch
-env CUDA_VISIBLE_DEVICES=1 python main_train_vid.py --test --batch-size 15 \
-  --model doanet_a2_wo_gcn --max-epoch 30 #--ckpt ./ckpt/doanet_a2_wo_gcn.pkl 
 
+pkill -9 python
+conda activate torch
+env CUDA_VISIBLE_DEVICES=1 python main_train_vid.py --batch-size 15 \
+  --model doanet_a2_wo_gcn --max-epoch 30 \
+  --ckpt ./ckpt/doanet_a2_wo_gcn_coco.pkl  >> ./log_out/tmp_sh.txt
