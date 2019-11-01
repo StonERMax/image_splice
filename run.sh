80ad95bc023e6758b74e5a7f2ed9c2a08a2348cf
asrafulashiq <asrafulashiq@gmail.com>
2019-11-01 15:43:30 -0400

    /

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@ conda activate torch
 set -x CUDA_VISIBLE_DEVICES 0
 
 git checkout ablation_1_aspp
-env CUDA_VISIBLE_DEVICES=0 python -m pdb main_template_match.py  \
+env CUDA_VISIBLE_DEVICES=0 python main_template_match.py  \
  --model aspp1 --suffix _exp --max-epoch 5
 
 git checkout ablation_1_aspp
-env CUDA_VISIBLE_DEVICES=0 python -m pdb main_train_temporal.py  --tune \
+env CUDA_VISIBLE_DEVICES=0 python main_train_temporal.py  --tune\
  --model aspp1 --suffix _exp --max-epoch 50 --ckpt ./ckpt/aspp1_coco_exp.pkl
+
+
+git checkout ablation_1_aspp
+env CUDA_VISIBLE_DEVICES=0 python main_train_temporal.py  --lr 1e-4 \
+ --model aspp1 --suffix _exp --max-epoch 50 --ckpt ./ckpt/temporal_aspp1_tmp_youtube_exp.pkl
