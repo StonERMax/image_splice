d9b85b6153a44d9007b02a34f5ee0bde79bb850c
asrafulashiq <asrafulashiq@gmail.com>
2019-11-02 01:34:28 -0400

    /

--- dataset_vid.py
+++ dataset_vid.py
@@ -489,13 +489,9 @@ class Dataset_vid(torch.utils.data.Dataset):
                     # positive match
                     t1 = np.random.choice(len(forge_indices) - t_max)
                     t2 = t1 + t_max
-                    forge_pos = forge_indices[t1 : t2]
-                    gt_pos = gt_indices[t1 : t2]
                 elif len(forge_indices) == t_max:
                     t1 = 0
                     t2 = len(forge_indices)
-                    forge_pos = forge_indices
-                    gt_pos = gt_indices
                 else:
                     # not enough forge frames as `t_max`
                     continue
@@ -505,7 +501,7 @@ class Dataset_vid(torch.utils.data.Dataset):
                     t1_n_gt = np.random.choice(
                         np.concatenate((
                             np.arange(0, gt_indices[t1]),
-                            np.arange(gt_indices[t1]+1, X.shape[0]-t_max)
+                            np.arange(gt_indices[t1]+1, X.shape[0]-t_max+1)
                         ))
                     )
                 except ValueError:
@@ -527,24 +523,23 @@ class Dataset_vid(torch.utils.data.Dataset):
                         # positive match
                         t1 = np.random.choice(len(forge_indices) - t_max)
                         t2 = t1 + t_max
-                        forge_pos = forge_indices[t1 : t2]
-                        gt_pos = gt_indices[t1 : t2]
                     elif len(forge_indices) == t_max:
                         t1 = 0
                         t2 = len(forge_indices)
-                        forge_pos = forge_indices
-                        gt_pos = gt_indices
                     else:
                         # not enough forge frames as `t_max`
                         continue
                     forge_neg = forge_indices[t1 : t2]
 
-                    t1_n_gt = np.random.choice(
-                        np.concatenate((
-                            np.arange(0, gt_indices[t1]),
-                            np.arange(gt_indices[t1]+1, X.shape[0]-t_max)
-                        ))
-                    )
+                    try:
+                        t1_n_gt = np.random.choice(
+                            np.concatenate((
+                                np.arange(0, gt_indices[t1]),
+                                np.arange(gt_indices[t1]+1, X.shape[0]-t_max+1)
+                            ))
+                        )
+                    except ValueError:
+                        continue
                     gt_neg = np.arange(t1_n_gt, t1_n_gt + t_max)
 
                     Xfn = X[forge_neg]
